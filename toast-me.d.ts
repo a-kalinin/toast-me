5e90670603e80ae9e662153b80b8db51c775e3df
Timothy Vane-Tempest <tim@vane-tempest.net>
2023-03-27 10:47:17 +0300

    Fix optional args.

--- toast-me.d.ts
+++ toast-me.d.ts
@@ -4,7 +4,7 @@ import { ToastActionType, ToastOptionsType } from './src/types';
 declare module 'toast-me' {
   export function toast(
     content: string,
-    receivedOptions: undefined | null | ToastOptionsType | 'error' | 'notify' = 'notify',
-    action: undefined | ToastActionType
+    receivedOptions?: null | ToastOptionsType | 'error' | 'notify' = 'notify',
+    action?: ToastActionType
   ): ToastMeClass
 }
