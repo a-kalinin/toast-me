0337565df51a2d535f04eb973a02418525d9e9cc
Timothy Vane-Tempest <tim@vane-tempest.net>
2023-03-27 10:54:37 +0300

    Remove initializer.

--- toast-me.d.ts
+++ toast-me.d.ts
@@ -4,7 +4,7 @@ import { ToastActionType, ToastOptionsType } from './src/types';
 declare module 'toast-me' {
   export default function toast(
     content: string,
-    receivedOptions?: null | ToastOptionsType | 'error' | 'notify' = 'notify',
+    receivedOptions?: null | ToastOptionsType | 'error' | 'notify',
     action?: ToastActionType
   ): ToastMeClass
 }
