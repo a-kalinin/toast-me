1b7605b824dfc80be146b5bb3aaa129ec7b9bc08
Alex Kalinin <alexkalinin.ru@gmail.com>
2026-08-25 20:09:00 +0700

    Update webpack-dev-server

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -1,37 +1,42 @@
-const webpackDevServer = require('webpack-dev-server');
-const webpack = require('webpack');
 const chalk = require('chalk');
 
 const port = process.env.PORT || 3005;
-const configDevEnv = require('./config/webpack.config.devEnv');
-const configToastMe = require('./config/webpack.config.devLib');
 
-let devIsRunning = false;
-const runDev = function() {
-  if (devIsRunning) return;
+async function main() {
+  const { default: webpack } = await import('webpack');
+  const { default: WebpackDevServer } = await import('webpack-dev-server');
 
-  const compiler = webpack(configDevEnv);
-  const devServerOptions = { ...configDevEnv.devServer, open: true };
-  const server = new webpackDevServer(devServerOptions, compiler);
+  const configDevEnv = require('./config/webpack.config.devEnv');
+  const configToastMe = require('./config/webpack.config.devLib');
 
-  (async () => {
-    await server.start();
+  let devIsRunning = false;
+  const runDev = function () {
+    if (devIsRunning) return;
+
+    const compiler = webpack(configDevEnv);
+    const devServerOptions = { ...configDevEnv.devServer, open: true };
+    const server = new WebpackDevServer(devServerOptions, compiler);
+
+    (async () => {
+      await server.start();
+      console.log();
+      console.log(chalk.green(`dev server listening on port ${port}`));
+      console.log();
+      devIsRunning = true;
+    })();
+  };
+
+  const compilerToastMe = webpack(configToastMe);
+
+  compilerToastMe.watch({}, function (err) {
+    if (err) {
+      console.error(err);
+      return;
+    }
     console.log();
-    console.log(chalk.green(`dev server listening on port ${port}`));
-    console.log();
-    devIsRunning = true;
-  })();
-};
+    console.log(chalk.blue('Compiler ToastMe cycle run successfully'));
+    runDev();
+  });
+}
 
-const compilerToastMe = webpack(configToastMe);
-
-compilerToastMe.watch({}, function(err) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log();
-  console.log(chalk.blue('Compiler ToastMe cycle run successfully'));
-  runDev();
-});
-
+main();
